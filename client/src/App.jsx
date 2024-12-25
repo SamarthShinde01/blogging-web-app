cb43cf15edf9cc87c30b9b08e18d14c1e0160e79
Samarth Shinde <samarthsshinde@gmail.com>
2024-12-25 12:53:56 +0530

    [client] navbar and hamburger menu

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Components/Navbar";
+
+function App() {
+	return (
+		<h1 className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+			{/* NAVBAR */}
+			<Navbar />
+			{/* BREADCRUMB */}
+			{/* INTRODUCTION */}
+			{/* FEATURED POSTS */}
+			{/* POST LIST */}
+		</h1>
+	);
+}
+
+export default App;
--- frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-function App() {
-	return (
-		<h1 className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">Hello World</h1>
-	);
-}
-
-export default App;
