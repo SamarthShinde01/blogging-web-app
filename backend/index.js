4cae8cd8bcd154009704d8b8c148ad2edeeccb25
Samarth Shinde <samarthsshinde@gmail.com>
2024-12-26 22:22:40 +0530

    [client] data fetching using react queru

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 /* Routes */
 import userRoutes from "./routes/user.route.js";
@@ -14,6 +15,7 @@ import { clerkMiddleware } from "@clerk/express";
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(cors(process.env.CLIENT_URL));
 app.use(clerkMiddleware());
 app.use("/webhook", webHookRoutes);
 
