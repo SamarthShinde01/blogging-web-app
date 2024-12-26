e163a8844f4ee5a3fc35fdfb0d893ecce38898b0
Samarth Shinde <samarthsshinde@gmail.com>
2024-12-26 14:48:45 +0530

    [client] post list page

--- client/src/routes/PostListPage.jsx
+++ client/src/routes/PostListPage.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import PostsList from "../Components/PostsList";
+import SideMenu from "../Components/SideMenu";
+
 const PostListPage = () => {
-	return <div>post list page</div>;
+	const [open, setOpen] = useState(false);
+
+	return (
+		<div>
+			<h1 className="mb-8 text-2xl">Development Blog</h1>
+			<button
+				onClick={() => setOpen((prev) => !prev)}
+				className="bg-blue-800 text-sm text-white px-4 py-2 rounded-2xl mb-4 md:hidden"
+			>
+				{open ? "Close" : "Filter or Search"}
+			</button>
+			<div className="flex flex-col-reverse gap-8 md:flex-row">
+				<div>
+					<PostsList />
+				</div>
+				<div className={`${open ? "block" : "hidden"} md:block `}>
+					<SideMenu />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default PostListPage;
